c596d627b3f45a04220dd950c50114bfe9d204a8
alveshenrique <alveshenrique@ymail.com>
2022-06-09 10:32:18 -0300

    CORS issue solved
    Solved issue where CORS didn't allow the frontend app to access the api

--- src/main/java/com/alveshenrique/geoalarm/controller/LocationController.java
+++ src/main/java/com/alveshenrique/geoalarm/controller/LocationController.java
@@ -3,12 +3,16 @@ package com.alveshenrique.geoalarm.controller;
 import com.alveshenrique.geoalarm.model.Location;
 import com.alveshenrique.geoalarm.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// Allow  Cross-Origin Resource Sharing (CORS) for the frontend app on localhost.
+// TODO CHANGE IT FOR PRODUCTION AND CHECK FOR BETTER OPTIONS WITH SPRING SECURITY
+@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/")
 public class LocationController {
